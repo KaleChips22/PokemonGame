627e3e44b6def7ad697ce8037a3ec99a6a65da0e
KaleChips22 <95776366+KaleChips22@users.noreply.github.com>
2022-08-22 07:56:53 -0400

    updated default raoute for dev

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var collection = JSON.parse(window.localStorage.getItem('collection')) || []
-var route = JSON.parse(window.localStorage.getItem('route')) || 'home'
+var route = JSON.parse(window.localStorage.getItem('route')) || 'test'
 
 document.getElementById('route').innerText = `Route: ${route}`
