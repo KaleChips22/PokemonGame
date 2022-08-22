119c9250e06cbbce5026b9a9918bbcdc6e94bcc7
KaleChips22 <95776366+KaleChips22@users.noreply.github.com>
2022-08-22 12:36:23 -0400

    fixed colors

--- pokemonList.js
+++ pokemonList.js
@@ -5,9 +5,9 @@ const pokemon = [
     p('Boash', 'red', 'darkgray', true, ['test', 'starter-b'], true),
     p('Vipyre', 'red', 'darkgray', true, ['test'], true),
     p('Pyraconda', 'red', 'darkgray', false, ['test'], true),
-    p('Swark', 'lightblue', 'aquamarine', true, ['test', 'starter-c'], true),
-    p('Finnife', 'lightblue', 'aquamarine', true, ['test'], true),
-    p('Sharkut', 'lightblue', 'aquamarine', false, ['test'], true),
+    p('Swark', 'lightblue', 'purple', true, ['test', 'starter-c'], true),
+    p('Finnife', 'lightblue', 'purple', true, ['test'], true),
+    p('Sharkut', 'lightblue', 'purple', false, ['test'], true),
 ]
 
 function p(name, color, shinyColor, canEvolve, routes, fakemon) {
