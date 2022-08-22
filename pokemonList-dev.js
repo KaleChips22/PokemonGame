47522f6e00293b5b43ee650d971f799febd2aa64
KaleChips22 <95776366+KaleChips22@users.noreply.github.com>
2022-08-22 08:02:56 -0400

    moved pokemonList to pokemonList-dev

---
+++ pokemonList-dev.js
@@ -0,0 +1,33 @@
+const pokemon = [
+    p('Bulbasaur', 'green', 'yellowgreen', true, ['test', 'starter-a'], false),
+    p('Ivysaur', 'green', 'yellowgreen', true, ['test', ], false),
+    p('Venusaur', 'green', 'yellowgreen', false, ['test', ], false),
+    p('Charmander', 'orange', 'yellow', true, ['test', 'starter-b'], false),
+    p('Charmeleon', 'orange', 'yellow', true, ['test', ], false),
+    p('Charizard', 'orange', 'darkgrey', false, ['test', ], false),
+    p('Squirtle', 'blue', 'lightblue', true, ['test', 'starter-c'], false),
+    p('Wartortle', 'blue', 'lightblue', true, ['test', ], false),
+    p('Blastoise', 'blue', 'purple', false, ['test', ], false),
+    p('Caterpie', 'green', 'yellow', true, ['test', 1, 2], false),
+    p('Metapod', 'green', 'orange', true, ['test', 2], false),
+    p('Butterfree', 'purple', 'purple', false, ['test', 5], false),
+    p('Weedle', 'tan', 'yellow', true, ['test', 1, 3], false),
+    p('Kakuna', 'yellow', 'green', true, ['test', 2, 3], false),
+    p('Beedrill', 'yellow', 'green', false, ['test', 5], false),
+    p('Pidgey', 'tan', 'yellowgreen', true, ['test', 1, 2, 3], false),
+    p('Pidgeotto', 'tan', 'yellowgreen', true, ['test', 2, 4], false),
+    p('Pidgeot', 'tan', 'yellowgreen', false, ['test', 5], false),
+    p('Rattata', 'purple', 'yellowgreen', true, ['test', 1, 2], false),
+    p('Raticate', 'tan', 'orange', false, ['test', 4, 5], false),
+]
+
+function p(name, color, shinyColor, canEvolve, routes, fakemon) {
+    return {
+        name,
+        color,
+        shinyColor,
+        canEvolve,
+        routes,
+        fakemon
+    }
+}
